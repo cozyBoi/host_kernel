bc70e4319c89e97a785d433acb70b60b2500b49a
cozyBoi <kjjh1126@gmail.com>
2021-12-29 19:29:44 +0900

    first commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 7 */
+/* SMP */
+#define UTS_MACHINE "x86_64"
+#define UTS_VERSION "#7 SMP Fri Aug 20 22:36:31 KST 2021"
+#define LINUX_COMPILE_BY "etri"
+#define LINUX_COMPILE_HOST "localhost.localdomain"
+#define LINUX_COMPILER "gcc version 8.4.1 20200928 (Red Hat 8.4.1-1) (GCC)"
---
+++ include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "4.18.0-240.10.1.el8.x86_64.emp"
